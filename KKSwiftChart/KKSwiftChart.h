529cfba6211a657d9dff47aec5f7678bc9f980ca
kamil <kkarpiak94@gmail.com>
2024-02-29 07:58:39 +0100

    Initial Commit

---
+++ KKSwiftChart/KKSwiftChart.h
@@ -0,0 +1,18 @@
+//
+//  KKSwiftChart.h
+//  KKSwiftChart
+//
+//  Created by Kamil Karpiak on 29/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KKSwiftChart.
+FOUNDATION_EXPORT double KKSwiftChartVersionNumber;
+
+//! Project version string for KKSwiftChart.
+FOUNDATION_EXPORT const unsigned char KKSwiftChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKSwiftChart/PublicHeader.h>
+
+
